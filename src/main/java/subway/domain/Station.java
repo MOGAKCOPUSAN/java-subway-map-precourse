5ea3c2eb0e4ed2fabf95d75cd7b94d3b5ad33c04
kim0914 <kosa0914@naver.com>
2022-12-16 13:53:14 +0900

    feat(Station): 인스턴스 검증 메서드 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,11 @@
 package subway.domain;
 
 public class Station {
+
     private String name;
 
     public Station(String name) {
+        validate(name);
         this.name = name;
     }
 
@@ -12,4 +14,9 @@ public class Station {
     }
 
     // 추가 기능 구현
+    public void validate(String name) {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException("지하철 역 이름은 2글자 이상이어야 합니다.");
+        }
+    }
 }
