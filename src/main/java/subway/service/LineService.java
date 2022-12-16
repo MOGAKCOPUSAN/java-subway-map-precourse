5df2bb29c0ee905382ea14947679d2a8bdc1acd0
kim0914 <kosa0914@naver.com>
2022-12-16 15:52:57 +0900

    feat(LineService): 새로운 노선 저장 메서드 추가

--- src/main/java/subway/service/LineService.java
+++ src/main/java/subway/service/LineService.java
@@ -1,13 +1,26 @@
 package subway.service;
 
 import subway.domain.Line;
+import subway.domain.Station;
 import subway.repository.LineRepository;
+import subway.repository.StationRepository;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LineService {
 
     public List<Line> getLines() {
         return LineRepository.lines();
+    }
+
+    public void add(String lineName, String upperStationName, String lowerStationName) {
+        Station upperStation = StationRepository.findByName(upperStationName);
+        Station lowerStation = StationRepository.findByName(lowerStationName);
+        if (Line.isDuplicated(lineName)) {
+            throw new IllegalArgumentException("이미 등록된 노선 이름입니다.");
+        }
+        Line line = new Line(lineName, Arrays.asList(upperStation, lowerStation));
+        line.add();
     }
 }
