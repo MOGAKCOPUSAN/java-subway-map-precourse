3bcace53fe493be2abb448030062875160d43cc4
This2sho <zx00018@naver.com>
2022-12-16 13:24:05 +0900

    feat(InputView): 등록할 역이름을 입력받는 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,6 +1,5 @@
 package subway.view;
 
-import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import subway.Constants;
@@ -13,6 +12,12 @@ public class InputView {
         Constants.INPUT_LINE_MANAGEMENT, Constants.INPUT_SECTION_MANAGEMENT,
         Constants.INPUT_SUBWAY_PRINT, Constants.INPUT_QUIT);
 
+    private static final Set<String> STATION_LINE_NUMBERS = Set.of(Constants.INPUT_REGISTRATION,
+        Constants.INPUT_DELETE, Constants.INPUT_SEARCH, Constants.INPUT_BACK);
+
+    private static final Set<String> SECTION_NUMBERS = Set.of(Constants.INPUT_REGISTRATION,
+        Constants.INPUT_DELETE, Constants.INPUT_BACK);
+    public static final int MIN_STATION_NAME_LENGTH = 2;
 
     public String readMainNumber() {
         System.out.println("## 메인 화면\n"
@@ -21,13 +26,13 @@ public class InputView {
             + "3. 구간 관리\n"
             + "4. 지하철 노선도 출력\n"
             + "Q. 종료");
-        printInputMessage();
+        printFeatureMessage();
         String input = scanner.nextLine();
         validateMainNumber(input);
         return input;
     }
 
-    private void printInputMessage() {
+    private void printFeatureMessage() {
         System.out.println("## 원하는 기능을 선택하세요.");
     }
 
@@ -36,4 +41,37 @@ public class InputView {
             throw new IllegalArgumentException("잘못된 기능 번호입니다.");
         }
     }
+
+    public String readStationNumber() {
+        System.out.println("## 역 관리 화면\n"
+            + "1. 역 등록\n"
+            + "2. 역 삭제\n"
+            + "3. 역 조회\n"
+            + "B. 돌아가기");
+        printFeatureMessage();
+        String input = scanner.nextLine();
+        validateStationAndLineNumber(input);
+        return input;
+    }
+
+    private void validateStationAndLineNumber(String input) {
+        if (!STATION_LINE_NUMBERS.contains(input)) {
+            throw new IllegalArgumentException("잘못된 기능 번호입니다.");
+        }
+    }
+
+    public String readStationName() {
+        System.out.println("## 등록할 역 이름을 입력하세요.\n");
+        String input = scanner.nextLine();
+        validateStationName(input);
+        return input;
+    }
+
+    private void validateStationName(String input) {
+        if (input.length() < MIN_STATION_NAME_LENGTH) {
+            throw new IllegalArgumentException("역 이름은 2글자 이상이어야 합니다.");
+        }
+    }
+
+
 }
