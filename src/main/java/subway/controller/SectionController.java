1aee409871c912c743d6ba817fb2ea01e6656399
kim0914 <kosa0914@naver.com>
2022-12-16 16:55:02 +0900

    feat(SectionController): 구간 사용자 입력 메서드 추가

---
+++ src/main/java/subway/controller/SectionController.java
@@ -0,0 +1,91 @@
+package subway.controller;
+
+import subway.service.SectionService;
+import subway.util.Log;
+import subway.view.OutputView;
+import subway.view.SectionInputView;
+
+import java.io.IOException;
+
+public class SectionController {
+
+    public static final String SECTION_REGISTER = "1";
+    public static final String SECTION_DELETE = "2";
+    public static final String BACK = "B";
+
+    private final SectionInputView sectionInputView = new SectionInputView();
+    private final SectionService sectionService = new SectionService();
+    private final OutputView outputView = new OutputView();
+
+    private String getUserChoice() {
+        try {
+            return sectionInputView.getSectionManageChoice();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getUserChoice();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getUserChoice();
+        }
+    }
+
+    private String getRegisterLine() {
+        try {
+            return sectionInputView.getRegisterSectionLine();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getRegisterLine();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getRegisterLine();
+        }
+    }
+
+    private int getSectionIndex() {
+        try {
+            return sectionInputView.getSectionIndex();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getSectionIndex();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getSectionIndex();
+        }
+    }
+
+    private String getRegisterStationName() {
+        try {
+            return sectionInputView.getRegisterSectionStation();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getRegisterStationName();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getRegisterStationName();
+        }
+    }
+
+    private String getDeleteLine() {
+        try {
+            return sectionInputView.getDeleteSectionLine();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getDeleteLine();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getDeleteLine();
+        }
+    }
+
+    private String getDeleteStationName() {
+        try {
+            return sectionInputView.getDeleteSectionStation();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getDeleteStationName();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getDeleteStationName();
+        }
+    }
+}
