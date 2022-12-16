48e304b442efcd25369a17b4470917ed1ed9d7a1
kim0914 <kosa0914@naver.com>
2022-12-16 16:52:47 +0900

    feat(StationController): 지하철역 사용자 입력 메서드 추가

---
+++ src/main/java/subway/controller/StationController.java
@@ -0,0 +1,44 @@
+package subway.controller;
+
+import subway.service.StationService;
+import subway.util.Log;
+import subway.view.OutputView;
+import subway.view.StationInputView;
+
+import java.io.IOException;
+
+public class StationController {
+
+    public static final String STATION_REGISTER = "1";
+    public static final String STATION_DELETE = "2";
+    public static final String STATION_INQUIRY = "3";
+    public static final String BACK = "B";
+
+    private final StationInputView stationInputView = new StationInputView();
+    private final StationService stationService = new StationService();
+    private final OutputView outputView = new OutputView();
+
+    private String getUserStationName() {
+        try {
+            return stationInputView.getResisterStationName();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getUserStationName();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getUserChoice();
+        }
+    }
+
+    private String getUserChoice() {
+        try {
+            return stationInputView.getStationManageChoice();
+        } catch (IOException e) {
+            Log.error(e.getMessage());
+            return getUserChoice();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return getUserChoice();
+        }
+    }
+}
