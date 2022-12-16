9e803667136d9c05acca7acbc429b0ad6b17cb23
kim0914 <kosa0914@naver.com>
2022-12-16 14:44:49 +0900

    feat(InitController): 노선 초기화 메서드 추가

--- src/main/java/subway/controller/InitController.java
+++ src/main/java/subway/controller/InitController.java
@@ -1,6 +1,8 @@
 package subway.controller;
 
+import subway.domain.Line;
 import subway.domain.Station;
+import subway.repository.StationRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,7 @@ public class InitController {
 
     public void init() {
         initStation();
+        initLine();
     }
 
     private void initStation() {
@@ -18,5 +21,32 @@ public class InitController {
                 .map(Station::new)
                 .collect(Collectors.toList());
         stations.forEach(Station::add);
+    }
+
+    private void initLine() {
+        line2Init();
+        line3Init();
+        lineBundangInit();
+    }
+
+    private void lineBundangInit() {
+        List<Station> lineBundangStations = Arrays.asList(StationRepository.findByName("강남역"),
+                StationRepository.findByName("양재역"), StationRepository.findByName("양재시민의숲역"));
+        Line lineBundang = new Line("신분당선", lineBundangStations);
+        lineBundang.add();
+    }
+
+    private void line3Init() {
+        List<Station> line3Stations = Arrays.asList(StationRepository.findByName("교대역"), StationRepository.findByName("남부터미널역"),
+                StationRepository.findByName("양재역"), StationRepository.findByName("매봉역"));
+        Line line3 = new Line("3호선", line3Stations);
+        line3.add();
+    }
+
+    private void line2Init() {
+        List<Station> line2Stations = Arrays.asList(StationRepository.findByName("교대역"),
+                StationRepository.findByName("강남역"), StationRepository.findByName("역삼역"));
+        Line line2 = new Line("2호선", line2Stations);
+        line2.add();
     }
 }
