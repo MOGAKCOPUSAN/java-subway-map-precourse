a8d7d7ed7e8b47a128c47e84d8cb804543eb390b
kim0914 <kosa0914@naver.com>
2022-12-16 16:46:35 +0900

    feat(OutputView): 지하철 노선도 출력 메서드 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -7,17 +7,20 @@ import java.util.List;
 
 public class OutputView {
 
-    private static String INPUT_SUCCESS_HEADER = "[INFO] ";
+    public static String INPUT_SUCCESS_HEADER = "[INFO] ";
 
-    private static String STATION_REGISTER_SUCCESS_MESSAGE = "지하철 역이 등록되었습니다.";
-    private static String STATION_DELETE_SUCCESS_MESSAGE = "지하철 역이 삭제되었습니다.";
-    private static String LINE_REGISTER_SUCCESS_MESSAGE = "지하철 노선이 등록되었습니다.";
-    private static String LINE_DELETE_SUCCESS_MESSAGE = "지하철 노선이 삭제되었습니다.";
-    private static String SECTION_REGISTER_SUCCESS_MESSAGE = "구간이 등록되었습니다.";
-    private static String SECTION_DELETE_SUCCESS_MESSAGE = "구간이 삭제되었습니다.";
+    public static String STATION_REGISTER_SUCCESS_MESSAGE = "지하철 역이 등록되었습니다.";
+    public static String STATION_DELETE_SUCCESS_MESSAGE = "지하철 역이 삭제되었습니다.";
+    public static String LINE_REGISTER_SUCCESS_MESSAGE = "지하철 노선이 등록되었습니다.";
+    public static String LINE_DELETE_SUCCESS_MESSAGE = "지하철 노선이 삭제되었습니다.";
+    public static String SECTION_REGISTER_SUCCESS_MESSAGE = "구간이 등록되었습니다.";
+    public static String SECTION_DELETE_SUCCESS_MESSAGE = "구간이 삭제되었습니다.";
 
-    private static String STATION_LIST_SHOW_MESSAGE = "## 역 목록";
-    private static String LINE_LIST_SHOW_MESSAGE = "## 노선 목록";
+    public static String STATION_LIST_SHOW_MESSAGE = "## 역 목록";
+    public static String LINE_LIST_SHOW_MESSAGE = "## 노선 목록";
+    public static String SUBWAY_INFO_SHOW_MESSAGE = "## 지하철 노선도";
+    public static String LINE_SEPARATOR = "---";
+    public static String LINE_CHANGER = "";
 
 
     public void showStations(List<Station> stations) {
@@ -28,6 +31,17 @@ public class OutputView {
     public void showLines(List<Line> lines) {
         printMessage(LINE_LIST_SHOW_MESSAGE);
         lines.forEach(line -> printMessage(INPUT_SUCCESS_HEADER + line.getName()));
+    }
+
+    public void shoeLineAndStation(List<Line> lines) {
+        printMessage(SUBWAY_INFO_SHOW_MESSAGE);
+        lines.forEach(line -> {
+            printMessage(INPUT_SUCCESS_HEADER + line.getName());
+            printMessage(INPUT_SUCCESS_HEADER + LINE_SEPARATOR);
+            line.getStations()
+                    .forEach(station -> printMessage(INPUT_SUCCESS_HEADER + station.getName()));
+            printMessage(LINE_CHANGER);
+        });
     }
 
     public void showStationRegisterResultMessage() {
