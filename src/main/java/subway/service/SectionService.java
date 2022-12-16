f33a5862d1f31c662a86f72743b62b478961c00a
kim0914 <kosa0914@naver.com>
2022-12-16 16:48:57 +0900

    feat(SectionService): 구간 삭제 시 크기 검증 메서드 추가

--- src/main/java/subway/service/SectionService.java
+++ src/main/java/subway/service/SectionService.java
@@ -7,6 +7,8 @@ import subway.repository.StationRepository;
 
 public class SectionService {
 
+    public static final int LINE_MIN_SIZE = 2;
+
     public void add(String lineName, String stationName, int index) {
         Line line = LineRepository.findByName(lineName);
         Station station = StationRepository.findByName(stationName);
@@ -18,6 +20,9 @@ public class SectionService {
         Station station = StationRepository.findByName(stationName);
         if (line.hasNotStation(station)) {
             throw new IllegalArgumentException("해당 노선이 해당 역을 가지고 있지 않습니다.");
+        }
+        if (line.getStations().size() < LINE_MIN_SIZE) {
+            throw new IllegalArgumentException("구간 삭제는 최소 3개 이상의 역이 존재하는 경우만 가능합니다.");
         }
         line.delete(station);
     }
