6dfcd58581c2fa78a174813ef7b26ffd96aad64e
kim0914 <kosa0914@naver.com>
2022-12-16 16:38:34 +0900

    fix(InitService): 노선 Array.asList -> arrayList 변경

--- src/main/java/subway/service/InitService.java
+++ src/main/java/subway/service/InitService.java
@@ -4,6 +4,7 @@ import subway.domain.Line;
 import subway.domain.Station;
 import subway.repository.StationRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,22 +31,22 @@ public class InitService {
     }
 
     private void lineBundangInit() {
-        List<Station> lineBundangStations = Arrays.asList(StationRepository.findByName("강남역"),
-                StationRepository.findByName("양재역"), StationRepository.findByName("양재시민의숲역"));
+        List<Station> lineBundangStations = new ArrayList<>(Arrays.asList(StationRepository.findByName("강남역"),
+                StationRepository.findByName("양재역"), StationRepository.findByName("양재시민의숲역")));
         Line lineBundang = new Line("신분당선", lineBundangStations);
         lineBundang.add();
     }
 
     private void line3Init() {
-        List<Station> line3Stations = Arrays.asList(StationRepository.findByName("교대역"), StationRepository.findByName("남부터미널역"),
-                StationRepository.findByName("양재역"), StationRepository.findByName("매봉역"));
+        List<Station> line3Stations = new ArrayList<>(Arrays.asList(StationRepository.findByName("교대역"), StationRepository.findByName("남부터미널역"),
+                StationRepository.findByName("양재역"), StationRepository.findByName("매봉역")));
         Line line3 = new Line("3호선", line3Stations);
         line3.add();
     }
 
     private void line2Init() {
-        List<Station> line2Stations = Arrays.asList(StationRepository.findByName("교대역"),
-                StationRepository.findByName("강남역"), StationRepository.findByName("역삼역"));
+        List<Station> line2Stations = new ArrayList<>(Arrays.asList(StationRepository.findByName("교대역"),
+                StationRepository.findByName("강남역"), StationRepository.findByName("역삼역")));
         Line line2 = new Line("2호선", line2Stations);
         line2.add();
     }
